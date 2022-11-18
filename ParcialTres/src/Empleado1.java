7c86997730fee596a3fe3fc787053a2ad8bc1d20
Camila Zamora <113726564+milacar2206@users.noreply.github.com>
2022-11-18 17:01:48 -0500

    Add files via upload

---
+++ ParcialTres/src/Empleado1.java
@@ -0,0 +1,24 @@
+//clase empleado
+public class Empleado1 {
+private String Nombre;
+private String Apellido;
+private String Dirrecion;
+private int DNI;
+private double Salario;
+private char Sexo;
+
+// construtor sobrecargado 
+public Empleado1(String Nombre) {
+ this.Nombre = Nombre;
+ System.out.println("Emplead" + Nombre);
+}
+
+ public void Empleado(String Apellido) {
+	 this.Apellido = Apellido;
+}{
+	
+
+	}
+	
+ }
+
